c57c6fc2e58f4393852a1242ab7c3ab98dc9f512
ngwatso <ngwatson79@yahoo.com>
2021-02-05 15:51:46 -0500

    MVP

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>Covid 19 Data</h1>
+			<h1>Covid-19 Data</h1>
 			<CovidData />
 		</div>
 	);
--- covid-19/src/components/CovidData.js
+++ covid-19/src/components/CovidData.js
@@ -7,8 +7,84 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 	useEffect(() => {});
 
 	const dataArray = [data];
-	// const dataArr = dataArray.splice(2, 1);
+
+	// const dataArr = dataArray[0];
 	const stateData = Object.entries(dataArray[0]);
+
+	const dataArr = [
+		0,
+		1,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		11,
+		12,
+		13,
+		16,
+		17,
+		18,
+		19,
+		20,
+		21,
+		22,
+		23,
+		24,
+		25,
+		26,
+		27,
+		28,
+		29,
+		30,
+		31,
+		32,
+		33,
+		34,
+		35,
+		36,
+		37,
+		38,
+		39,
+		41,
+		42,
+		43,
+		44,
+		47,
+		48,
+		49,
+		50,
+		51,
+		53,
+		54,
+		56,
+		57,
+		58,
+		59,
+		60,
+	];
+
+	const dataArr2 = [2];
+
+	const totalData = stateData
+		.sort()
+		.filter((el, i) => dataArr2.some((j) => i === j));
+
+	// stateData.filter((el, i) => dataArr.some((j) => i === j));
+
+	// const removeItems = (arr, remove) => {
+	// 	return arr.filter((value) => !remove.includes(value));
+	// };
+
+	// console.log(
+	// 	"newArray =====> ",
+
+	// );
+
+	const newArray = stateData
+		.sort()
+		.filter((el, i) => dataArr.some((j) => i === j));
 
 	const handleGetData = (e) => {
 		e.preventDefault();
@@ -16,7 +92,7 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 	};
 
 	if (isFetching) {
-		return <h2>Fetching Data...</h2>;
+		return <h2>Fetching Data, one moment...</h2>;
 	}
 	console.log("Covid Data: stateData =====> ", stateData);
 	console.log("Covid Data: dataArray =====> ", dataArray[0]);
@@ -29,54 +105,84 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 
 	return (
 		<>
-			<h2>Current Data By State (updated hourly):</h2>
-			<button onClick={handleGetData}>Get Recent Data</button>
-			<div className="container-state-data">
-				{/* <div
+			<div className="app">
+				<h2>Current Data By State (updated hourly):</h2>
+				<button onClick={handleGetData}>Get Recent Data</button>
+				{error && (
+					<div>
+						<h2 style={{ color: "red" }}>
+							Error: {error}. Please try again later.
+						</h2>
+					</div>
+				)}
+				<div className="us-container">
+					<div className="us-data">
+						{totalData.map((item) => {
+							return (
+								<div key={item} className="us-card">
+									<div className="state-name">
+										{/* {item[0]} */}
+										United States
+										{/* {Object.keys(dataArray[0]).map(
+								(name) => name === item
+							)} */}
+										{/* {Object.getOwnPropertyNames(item)} */}
+										{/* {Object.defineProperty(item)} */}
+										{/* Michigan */}
+									</div>
+									<div className="confirmed-cases">
+										Confirmed: {item[1].confirmed}
+									</div>
+									<div className="deaths">
+										Deaths: {item[1].deaths}
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+				<div className="container-state-data">
+					{/* <div
 					// key={Object.getOwnPropertyNames(data)}
 					key={Date.now()}
 					className="state-data"
 				> */}
-				{/* {stateNames.forEach((name) => {
+					{/* {stateNames.forEach((name) => {
 					dataArray.name.map((item) => { */}
-				{/* {for ( let i = 0; i < stateNames.length; i++ ) { */}
-				{stateData.sort().map((item) => {
-					/* {Object.keys(dataArray[0]).map((item) => { */
+					{/* {for ( let i = 0; i < stateNames.length; i++ ) { */}
+					{newArray.map((item) => {
+						/* {Object.keys(dataArray[0]).map((item) => { */
 
-					// console.log("item =====> ", item);
-					return (
-						<div key={item} className="state-card">
-							<div className="state-name">
-								{item[0]}
-								{/* {Object.keys(dataArray[0]).map(
+						// console.log("item =====> ", item);
+						return (
+							<div key={item} className="state-card">
+								<div className="state-name">
+									{item[0]}
+									{/* {Object.keys(dataArray[0]).map(
 									(name) => name === item
 								)} */}
-								{/* {Object.getOwnPropertyNames(item)} */}
-								{/* {Object.defineProperty(item)} */}
-								{/* Michigan */}
+									{/* {Object.getOwnPropertyNames(item)} */}
+									{/* {Object.defineProperty(item)} */}
+									{/* Michigan */}
+								</div>
+								<div className="confirmed-cases">
+									Confirmed: {item[1].confirmed}
+								</div>
+								<div className="deaths">
+									Deaths: {item[1].deaths}
+								</div>
 							</div>
-							<div className="confirmed-cases">
-								Confirmed: {item[1].confirmed}
-							</div>
-							<div className="deaths">
-								Deaths: {item[1].deaths}
-							</div>
-						</div>
-					);
-				})}
-				{/* </div> */}
-			</div>
-			{/* <StateData getCovidData={getCovidData} /> */}
-			{/* <div key={data.key} className="state-data">
+						);
+					})}
+					{/* </div> */}
+				</div>
+				{/* <StateData getCovidData={getCovidData} /> */}
+				{/* <div key={data.key} className="state-data">
 				{data.map((item) => {
 					return <div className="state-name"></div>;
 				})}
 			</div> */}
-			{error && (
-				<div>
-					<h2 style={{ color: "red" }}>Error: {error}</h2>
-				</div>
-			)}
+			</div>
 		</>
 	);
 };
