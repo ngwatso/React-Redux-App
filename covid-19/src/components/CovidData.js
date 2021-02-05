7a09adbb3403fa318102fda4eb3a52c7add48074
ngwatso <ngwatson79@yahoo.com>
2021-02-05 13:03:25 -0500

    only styling left to do

--- covid-19/src/components/CovidData.js
+++ covid-19/src/components/CovidData.js
@@ -7,7 +7,8 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 	useEffect(() => {});
 
 	const dataArray = [data];
-	const stateNames = Object.keys(data);
+	// const dataArr = dataArray.splice(2, 1);
+	const stateData = Object.entries(dataArray[0]);
 
 	const handleGetData = (e) => {
 		e.preventDefault();
@@ -17,13 +18,19 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 	if (isFetching) {
 		return <h2>Fetching Data...</h2>;
 	}
-	console.log("Covid Data: stateNames =====> ", stateNames);
+	console.log("Covid Data: stateData =====> ", stateData);
 	console.log("Covid Data: dataArray =====> ", dataArray[0]);
-	console.log("Object.keys =====> ", Object.keys(dataArray[0]));
+	// console.log("dataArr =====> ", dataArr);
+	// console.log("Object.keys =====> ", Object.keys(dataArray[0]));
+	console.log(
+		"Object.getOwnPropertyDescriptor =====> ",
+		Object.getOwnPropertyDescriptor(dataArray)
+	);
 
 	return (
 		<>
 			<h2>Current Data By State (updated hourly):</h2>
+			<button onClick={handleGetData}>Get Recent Data</button>
 			<div className="container-state-data">
 				{/* <div
 					// key={Object.getOwnPropertyNames(data)}
@@ -33,19 +40,26 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 				{/* {stateNames.forEach((name) => {
 					dataArray.name.map((item) => { */}
 				{/* {for ( let i = 0; i < stateNames.length; i++ ) { */}
-				{Object.keys(dataArray[0]).map((item) => {
+				{stateData.sort().map((item) => {
+					/* {Object.keys(dataArray[0]).map((item) => { */
+
+					// console.log("item =====> ", item);
 					return (
 						<div key={item} className="state-card">
 							<div className="state-name">
+								{item[0]}
+								{/* {Object.keys(dataArray[0]).map(
+									(name) => name === item
+								)} */}
 								{/* {Object.getOwnPropertyNames(item)} */}
-								{Object.keys(dataArray[0])}
+								{/* {Object.defineProperty(item)} */}
 								{/* Michigan */}
 							</div>
 							<div className="confirmed-cases">
-								Confirmed: {item.confirmed}
+								Confirmed: {item[1].confirmed}
 							</div>
 							<div className="deaths">
-								Deaths: {item.deaths}
+								Deaths: {item[1].deaths}
 							</div>
 						</div>
 					);
@@ -63,7 +77,6 @@ const CovidData = ({ data, isFetching, error, getCovidData }) => {
 					<h2 style={{ color: "red" }}>Error: {error}</h2>
 				</div>
 			)}
-			<button onClick={handleGetData}>Get Recent Data</button>
 		</>
 	);
 };
