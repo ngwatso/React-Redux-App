d004ea78329c0141f1b7bbc32382f4c408913ff9
ngwatso <ngwatson79@yahoo.com>
2021-02-18 10:29:14 -0500

    updated axios.get

--- covid-19/src/actions/index.js
+++ covid-19/src/actions/index.js
@@ -1,20 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
 // https://covid-api.mmediagroup.fr/v1
 
-export const GET_COVID_DATA = "GET_COVID_DATA";
-export const START_FETCHING = "START_FETCHING";
-export const FETCH_FAILED = "FETCH_FAILED";
+export const GET_COVID_DATA = 'GET_COVID_DATA';
+export const START_FETCHING = 'START_FETCHING';
+export const FETCH_FAILED = 'FETCH_FAILED';
 
 export const getCovidData = () => {
 	return (dispatch) => {
 		dispatch({ type: START_FETCHING });
 
-		axios.get(
-			"https://cors-anywhere.herokuapp.com/https://covid-api.mmediagroup.fr/v1/cases"
-		)
+		axios.get('https://covid-api.mmediagroup.fr/v1/cases')
 			.then((res) => {
-				console.log("API DATA =====> ", res.data.US);
+				console.log('API DATA =====> ', res.data.US);
 				// console.log(
 				// 	"ObjectNames =====> ",
 				// 	Object.getOwnPropertyNames(res.data.US)
@@ -25,7 +23,7 @@ export const getCovidData = () => {
 				});
 			})
 			.catch((err) => {
-				console.error("ERROR PULLING DATA", err.message);
+				console.error('ERROR PULLING DATA', err.message);
 				dispatch({ type: FETCH_FAILED, payload: err.message });
 			});
 	};
